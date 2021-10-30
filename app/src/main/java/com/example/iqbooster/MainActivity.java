6a7276a3f7ffe04ebab54aea820200781663e6b1
Zerong Li <zawinglee@Zerongs-MacBook-Pro.local>
2021-10-30 15:17:26 -0700

    connect firebase and add drawer

--- app/src/main/java/com/example/iqbooster/MainActivity.java
+++ app/src/main/java/com/example/iqbooster/MainActivity.java
@@ -1,14 +1,124 @@
 package com.example.iqbooster;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.navigation.NavigationView;
+
+import java.util.Objects;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    private static final String TAG = "MainActivity: ";
+
+    private Toolbar mToolbar;
+    private DrawerLayout mDrawerLayout;
+    private NavigationView mNavigationView;
+    private ActionBarDrawerToggle mDrawerToggle;
+
+    // drawer stuff
+    private View mheaderView;
+    private ImageView mProfilePic;
+    private TextView mUsername;
+    private TextView mEmailAddress;
+    private MenuItem mNewsFeedItem;
+    private MenuItem mPostItem;
+    private MenuItem mCollectItem;
+    private MenuItem mLogoutItem;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.drawer_layout);
+
+        mToolbar = (Toolbar) findViewById(R.id.main_toolbar);
+        setSupportActionBar(mToolbar);
+        Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
+
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mNavigationView = (NavigationView) findViewById(R.id.drawer_view);
+
+        mDrawerToggle = new ActionBarDrawerToggle(
+                this,
+                mDrawerLayout,
+                mToolbar,
+                R.string.open,
+                R.string.close
+        );
+
+        mDrawerLayout.addDrawerListener(mDrawerToggle);
+        mDrawerToggle.syncState();;
+        mNavigationView.setNavigationItemSelectedListener(this);
+
+        mheaderView = mNavigationView.getHeaderView(0);
+        mProfilePic = (ImageView) mheaderView.findViewById(R.id.drawer_avatar);
+        // TODO: add profile listener here...
+        mUsername = (TextView) mheaderView.findViewById(R.id.drawer_username);
+        mEmailAddress = (TextView) mheaderView.findViewById(R.id.drawer_email);
+        mNewsFeedItem = (MenuItem) mNavigationView.getMenu().getItem(0);
+        mPostItem = (MenuItem) mNavigationView.getMenu().getItem(1);
+        mCollectItem = (MenuItem) (MenuItem) mNavigationView.getMenu().getItem(2);
+        mLogoutItem = (MenuItem) mNavigationView.getMenu().getItem(3);
+
+        mNewsFeedItem.setChecked(true);
+        getSupportActionBar().setTitle("News Feed");
+
+    }
+
+    @Override
+    protected void onStart() {
+        // TODO: add firebase ref here
+        super.onStart();
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (mDrawerLayout.isDrawerOpen(GravityCompat.START)) {
+            mDrawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        if (id == mNewsFeedItem.getItemId()) {
+            getSupportActionBar().setTitle("News Feed");
+        } else if (id == mPostItem.getItemId()) {
+        } else if (id == mCollectItem.getItemId()) {
+        } else if (id == mLogoutItem.getItemId()) {
+        }
+
+        closeDrawer();
+        return true;
+    }
+
+    private void closeDrawer() {
+        if (mDrawerLayout.isDrawerOpen(GravityCompat.START)) {
+            mDrawerLayout.closeDrawer(GravityCompat.START);
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if (mDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
