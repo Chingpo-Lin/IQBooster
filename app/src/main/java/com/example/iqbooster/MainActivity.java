34ae7c3ab2f286ee038353456b93840990c00daa
zawinglee <jerry.824.li@gmail.com>
2021-11-23 21:56:29 -0800

    fixed a problem which makes user profile image does not load correctly

--- app/src/main/java/com/example/iqbooster/MainActivity.java
+++ app/src/main/java/com/example/iqbooster/MainActivity.java
@@ -65,7 +65,7 @@ import de.hdodenhof.circleimageview.CircleImageView;
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener, ActivityInterface, SensorEventListener {
 
     private static final String TAG = "MainActivity";
-    private static final String BUILD_VERSION = "Current Build Version: 1.3.000";
+    private static final String BUILD_VERSION = "Current Build Version: 1.3.001";
 
     private MaterialToolbar mToolbar;
     private DrawerLayout mDrawerLayout;
